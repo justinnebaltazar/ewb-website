6388612ed33d888f03a1af560683cb74e58171f3
Justinne Baltazar <jfb3@sfu.ca>
2025-02-02 21:29:12 -0800

    Set up file organization, CSS, and initial components - Remove default Vite and React set up - Upload logos and button images - Create Navbar component and add styling

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import styles from "../Navbar/Navbar.module.css"
+
+export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <img className={styles.logo} src="./images/logos/ewb-logo.png" alt="ewb-logo" />
+            </div>
+            <div className={styles.menu}>
+                <img className={styles.menuBtn} 
+                src={menuOpen ? "./images/navbar/closeBtn.png" : "./images/navbar/menuBtn.png"} 
+                alt="menu-button" 
+                onClick={() => {
+                    setMenuOpen(!menuOpen)
+                }}/>
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => {setMenuOpen(false)}}>
+                    <li>About us</li>
+                    <li>What we do</li>
+                    <li>Get involved</li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
