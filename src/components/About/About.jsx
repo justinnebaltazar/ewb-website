94ab3b7bfcd832c0410d6818e5ba280d8e308891
Justinne Baltazar <jfb3@sfu.ca>
2025-02-28 20:58:14 -0800

    Add styling to about component and create json file with goal info

---
+++ src/components/About/About.jsx
@@ -0,0 +1,25 @@
+import styles from "../About/About.module.css"
+import goals from "../../data/about.json"
+
+export const About = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.quotes}>
+                <h3>Engineers Without Borders Canada stands at the forefront of addressing global challenges through engineering and technological innovation, “engineering an equitable and sustainable future for marginalized people and the planet.”</h3>
+                <h3>Our focus on clean technologies, sustainable agriculture and food systems, healthcare resilience in the face of climate change, and education and job creation in STEM fields are pillars of our approach.</h3>
+                <h3></h3>
+            </div>
+            <div className={styles.goalsContainer}>
+                {goals.map((goal, id) => (
+                    <div key={id} className={styles.goal}>
+                        <h2 className={styles.title}>{goal.title}</h2>
+                        <h4 className={styles.subheader}>{goal.subheading}</h4>
+                        <p className={styles.desc}>{goal.description}</p>
+                        <div className={styles.button}>Find out more!</div>
+                    </div>
+                ))}
+            </div>
+        </section>
+        
+    )
+}
